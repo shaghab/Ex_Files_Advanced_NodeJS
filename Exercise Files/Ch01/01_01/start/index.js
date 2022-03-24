eaf278359fcee0354540004a78971c8f0497884f
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-24 11:43:29 +0500

    6.1 sequentially delay callbacks

--- Exercise Files/Ch01/01_01/start/index.js
+++ Exercise Files/Ch01/01_01/start/index.js
@@ -6,4 +6,10 @@ console.log("starting delays");
 
 delay(2, () => {
   console.log("2 seconds");
+  delay(1, () => {
+    console.log("3 seconds");
+    delay(1, () => {
+      console.log("4 seconds");
+    });
+  });
 });
