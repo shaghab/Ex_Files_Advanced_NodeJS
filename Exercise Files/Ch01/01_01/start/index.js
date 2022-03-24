24846d7606faffd87b17de7157c3168e695f9627
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-24 11:41:24 +0500

    6.1 using setTimeout for async func definitions

--- Exercise Files/Ch01/01_01/start/index.js
+++ Exercise Files/Ch01/01_01/start/index.js
@@ -1,11 +1,9 @@
-function hideString(str, done) {
-  process.nextTick(() => {
-    done(str.replace(/[a-zA-Z]/g, "X"));
-  });
+function delay(seconds, callback) {
+  setTimeout(callback, seconds * 1000);
 }
 
-hideString("Hello World", (hidden) => {
-  console.log(hidden);
-});
+console.log("starting delays");
 
-console.log("end");
+delay(2, () => {
+  console.log("2 seconds");
+});
