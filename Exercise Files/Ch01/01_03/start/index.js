203c208a78f48332a563bb93de43755fc51181a0
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-26 16:56:07 +0500

    1.3. using reject method

--- Exercise Files/Ch01/01_03/start/index.js
+++ Exercise Files/Ch01/01_03/start/index.js
@@ -1,13 +1,15 @@
 var delay = (seconds) =>
   new Promise((resolves, rejects) => {
-    throw new Error("Argh");
+    if (seconds > 3) {
+      rejects(new Error(`${seconds} is too long!`));
+    }
 
     setTimeout(() => {
       resolves("the long delay has ended");
     }, seconds * 1000);
   });
 
-delay(1)
+delay(4)
   .then(console.log)
   .then(() => 42)
   .then((number) => console.log(`Hello world: ${number}`))
