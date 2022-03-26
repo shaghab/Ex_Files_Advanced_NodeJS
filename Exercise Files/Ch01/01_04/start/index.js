38129d7a5db41c2e29daf13c597df79ad1036858
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-26 23:41:54 +0500

    1.4 promisifying fs.writeFile to make it easier to with with callbacls

--- Exercise Files/Ch01/01_04/start/index.js
+++ Exercise Files/Ch01/01_04/start/index.js
@@ -1,18 +1,9 @@
+var fs = require("fs");
+
 var { promisify } = require("util");
 
-var delay = (seconds, callback) => {
-  if (seconds > 3) {
-    callback(new Error(`${seconds} seconds it too long!`));
-  } else {
-    setTimeout(
-      () => callback(null, `the ${seconds} second delay is over.`),
-      seconds
-    );
-  }
-};
+var writeFile = promisify(fs.writeFile);
 
-var promiseDelay = promisify(delay);
-
-promiseDelay(2)
-  .then(console.log)
-  .catch((error) => console.log(`error: ${error.message}`));
+writeFile("sample.txt", "This is a sample")
+  .then(() => console.log("file successfully created"))
+  .catch((error) => console.log("error creating file"));
