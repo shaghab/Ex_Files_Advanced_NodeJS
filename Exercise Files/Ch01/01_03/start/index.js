299ff50e5b9480b44bcb6517133ec042a14aa913
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-26 16:53:45 +0500

    1.3 throwing and catching errors

--- Exercise Files/Ch01/01_03/start/index.js
+++ Exercise Files/Ch01/01_03/start/index.js
@@ -1,5 +1,7 @@
 var delay = (seconds) =>
   new Promise((resolves, rejects) => {
+    throw new Error("Argh");
+
     setTimeout(() => {
       resolves("the long delay has ended");
     }, seconds * 1000);
@@ -8,6 +10,7 @@ var delay = (seconds) =>
 delay(1)
   .then(console.log)
   .then(() => 42)
-  .then((number) => console.log(`Hello world: ${number}`));
+  .then((number) => console.log(`Hello world: ${number}`))
+  .catch((error) => console.log(`error: ${error.message}`));
 
 console.log("end first tick");
