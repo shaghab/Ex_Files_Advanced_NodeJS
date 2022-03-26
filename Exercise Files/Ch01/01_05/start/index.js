6a7e43a4b2b7b1171cc10816d24602fbb1482be0
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-26 23:59:44 +0500

    1.5 removign callback hell with promises

--- Exercise Files/Ch01/01_05/start/index.js
+++ Exercise Files/Ch01/01_05/start/index.js
@@ -1,33 +1,29 @@
 var fs = require("fs");
+
+var { promisify } = require("util");
+var writeFile = promisify(fs.writeFile);
+var unlink = promisify(fs.unlink);
+
 var beep = () => process.stdout.write("\x07");
 
-const doStuffSequentially = () => {
-  console.log("starting");
-  setTimeout(() => {
-    console.log("waiting");
-    setTimeout(() => {
-      console.log("waiting some more");
-      fs.writeFile("file.txt", "Sample File...", (error) => {
-        if (error) {
-          console.error(error);
-        } else {
-          beep();
-          console.log("file.txt created");
-          setTimeout(() => {
-            beep();
-            fs.unlink("file.txt", (error) => {
-              if (error) {
-                console.error(error);
-              } else {
-                console.log("file.txt removed");
-                console.log("sequential execution complete");
-              }
-            });
-          }, 3000);
-        }
-      });
-    }, 2000);
-  }, 1000);
-};
+var delay = (seconds) =>
+  new Promise((resolves) => {
+    setTimeout(resolves, seconds * 1000);
+  });
+
+const doStuffSequentially = () =>
+  Promise.resolve()
+    .then(() => console.log("starting"))
+    .then(() => delay(1))
+    .then(() => console.log("waiting"))
+    .then(() => delay(2))
+    .then(() => writeFile("file.txt", "Sample File..."))
+    .then(beep)
+    .then(() => console.log("file.txt created"))
+    .then(() => delay(3))
+    .then(() => unlink("file.txt"))
+    .then(beep)
+    .then(() => console.log("file.txt removed"))
+    .catch(console.error);
 
 doStuffSequentially();
