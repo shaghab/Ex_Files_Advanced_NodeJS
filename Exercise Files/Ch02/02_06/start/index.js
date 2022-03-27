c5f4e71139947d33cb314f68d407ebdf32b61189
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-27 20:35:02 +0500

    piping stdin to txt file

--- Exercise Files/Ch02/02_06/start/index.js
+++ Exercise Files/Ch02/02_06/start/index.js
@@ -1,6 +1,5 @@
-const { createReadStream, createWriteStream } = require("fs");
+const { createWriteStream } = require("fs");
 
-const readStream = createReadStream("../../powder-day.mp4");
-const writeStream = createWriteStream("./copy.mp4");
+const writeStream = createWriteStream("./file.txt");
 
-readStream.pipe(writeStream).on("error", console.error);
+process.stdin.pipe(writeStream);
