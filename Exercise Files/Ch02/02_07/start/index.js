04b71af2e5f0e71c913f3e43da90f96bc2ec97b8
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-27 20:55:49 +0500

    tracking how much data is passed through using a PassThrough

--- Exercise Files/Ch02/02_07/start/index.js
+++ Exercise Files/Ch02/02_07/start/index.js
@@ -1,6 +1,16 @@
+const { PassThrough } = require("stream");
+
 const { createReadStream, createWriteStream } = require("fs");
 
 const readStream = createReadStream("../../powder-day.mp4");
 const writeStream = createWriteStream("./copy.mp4");
 
-readStream.pipe(writeStream);
+const report = new PassThrough();
+
+var total = 0;
+report.on("data", (chunk) => {
+  total += chunk.length;
+  console.log("bytes: ", total);
+});
+
+readStream.pipe(report).pipe(writeStream);
