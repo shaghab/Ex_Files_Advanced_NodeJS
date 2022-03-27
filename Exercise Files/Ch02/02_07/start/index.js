161f1ce2e0265ae50d0ed7c16a5a80e6ce3d25a9
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-27 20:36:44 +0500

    formatted

--- Exercise Files/Ch02/02_07/start/index.js
+++ Exercise Files/Ch02/02_07/start/index.js
@@ -1,6 +1,6 @@
-const { createReadStream, createWriteStream } = require('fs');
+const { createReadStream, createWriteStream } = require("fs");
 
-const readStream = createReadStream('../../powder-day.mp4')
-const writeStream = createWriteStream('./copy.mp4');
+const readStream = createReadStream("../../powder-day.mp4");
+const writeStream = createWriteStream("./copy.mp4");
 
 readStream.pipe(writeStream);
