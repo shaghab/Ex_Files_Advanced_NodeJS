6efe32da62c885f52e96c39347bde6db06760831
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-27 22:19:43 +0500

    how big the file is

--- Exercise Files/Ch03/03_01/start/index.js
+++ Exercise Files/Ch03/03_01/start/index.js
@@ -1,8 +1,14 @@
 const { createServer } = require("http");
-const { createReadStream } = require("fs");
+const { stat, createReadStream } = require("fs");
+const { promisify } = require("util");
 const filename = "../../powder-day.mp4";
+const fileInfo = promisify(stat);
 
-createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "video/mp4" });
+createServer(async (req, res) => {
+  const { size } = await fileInfo(filename);
+  res.writeHead(200, {
+    "content-Length": size,
+    "Content-Type": "video/mp4",
+  });
   createReadStream(filename).pipe(res);
 }).listen(3000, () => console.log("server running - 3000"));
