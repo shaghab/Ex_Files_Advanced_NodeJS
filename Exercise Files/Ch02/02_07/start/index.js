d727d90e861dfea102ec88db3df59a9c10618040
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-27 21:18:55 +0500

    throttle added

--- Exercise Files/Ch02/02_07/start/index.js
+++ Exercise Files/Ch02/02_07/start/index.js
@@ -1,11 +1,30 @@
-const { PassThrough } = require("stream");
+const { PassThrough, Duplex } = require("stream");
 
 const { createReadStream, createWriteStream } = require("fs");
 
 const readStream = createReadStream("../../powder-day.mp4");
 const writeStream = createWriteStream("./copy.mp4");
 
+class Throttle extends Duplex {
+  constructor(ms) {
+    super();
+    this.delay = ms;
+  }
+
+  _write(chunk, encoding, callback) {
+    this.push(chunk);
+    setTimeout(callback, this.delay);
+  }
+
+  _read() {}
+
+  _final() {
+    this.push(null);
+  }
+}
+
 const report = new PassThrough();
+const throttle = new Throttle(100);
 
 var total = 0;
 report.on("data", (chunk) => {
@@ -13,4 +32,4 @@ report.on("data", (chunk) => {
   console.log("bytes: ", total);
 });
 
-readStream.pipe(report).pipe(writeStream);
+readStream.pipe(throttle).pipe(report).pipe(writeStream);
