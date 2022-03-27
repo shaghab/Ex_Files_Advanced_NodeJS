7df23197108c0612b55cd0c77f6f93309e00a3dd
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-27 13:26:51 +0500

    1.9 added missing starting code from prev exc

--- Exercise Files/Ch01/01_09/start/index.js
+++ Exercise Files/Ch01/01_09/start/index.js
@@ -15,3 +15,31 @@ var tasks = [
   delay(3),
   delay(5),
 ];
+
+class PromiseQueue {
+  constructor(promises = [], concurrentCount = 1) {
+    this.concurrent = concurrentCount;
+    this.total = promises.length;
+    this.todo = promises;
+    this.running = [];
+    this.complete = [];
+  }
+
+  get runAnother() {
+    return this.running.length < this.concurrent && this.todo.length;
+  }
+
+  run() {
+    while (this.runAnother) {
+      var promise = this.todo.shift();
+      promise.then(() => {
+        this.complete.push(this.running.shift());
+        this.run();
+      });
+      this.running.push(promise);
+    }
+  }
+}
+
+var delayQueue = new PromiseQueue(tasks, 2);
+delayQueue.run();
