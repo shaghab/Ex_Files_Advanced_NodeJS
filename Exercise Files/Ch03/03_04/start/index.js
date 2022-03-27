135f13c2ded43d37ca074753b965f7851e6dfb47
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-27 23:31:05 +0500

    formatted

--- Exercise Files/Ch03/03_04/start/index.js
+++ Exercise Files/Ch03/03_04/start/index.js
@@ -1,45 +1,41 @@
-const { createServer } = require('http');
-const {
-  stat,
-  createReadStream,
-  createWriteStream 
-} = require('fs');
-const { promisify } = require('util');
-const fileName = '../../powder-day.mp4';
+const { createServer } = require("http");
+const { stat, createReadStream, createWriteStream } = require("fs");
+const { promisify } = require("util");
+const fileName = "../../powder-day.mp4";
 const fileInfo = promisify(stat);
 
 const respondWithVideo = async (req, res) => {
   const { size } = await fileInfo(fileName);
   const range = req.headers.range;
   if (range) {
-    let [start, end] = range.replace(/bytes=/, '').split('-');
+    let [start, end] = range.replace(/bytes=/, "").split("-");
     start = parseInt(start, 10);
     end = end ? parseInt(end, 10) : size - 1;
     res.writeHead(206, {
-       'Content-Range': `bytes ${start}-${end}/${size}`,
-       'Accept-Ranges': 'bytes',
-       'Content-Length': (end-start) + 1,
-       'Content-Type': 'video/mp4'
-    })
+      "Content-Range": `bytes ${start}-${end}/${size}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": end - start + 1,
+      "Content-Type": "video/mp4",
+    });
     createReadStream(fileName, { start, end }).pipe(res);
   } else {
     res.writeHead(200, {
-      'Content-Length': size,
-      'Content-Type': 'video/mp4'
+      "Content-Length": size,
+      "Content-Type": "video/mp4",
     });
     createReadStream(fileName).pipe(res);
   }
-}
+};
 
 createServer((req, res) => {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     req.pipe(res);
     req.pipe(process.stdout);
-    req.pipe(createWriteStream('./upload.file'));
-  } else if (req.url === '/video') {
+    req.pipe(createWriteStream("./upload.file"));
+  } else if (req.url === "/video") {
     respondWithVideo(req, res);
   } else {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.writeHead(200, { "Content-Type": "text/html" });
     res.end(`
       <form enctype="multipart/form-data" method="POST" action="/">
         <input type="file" name="upload-file" />
@@ -47,5 +43,4 @@ createServer((req, res) => {
       </form>
     `);
   }
-
-}).listen(3000, () => console.log('server running - 3000'));
+}).listen(3000, () => console.log("server running - 3000"));
