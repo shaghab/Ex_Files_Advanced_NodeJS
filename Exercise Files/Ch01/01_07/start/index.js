ea5244cd7d3806a00ea528190196a19ed52bf7bf
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-27 11:38:06 +0500

    1.7 parallel execution

--- Exercise Files/Ch01/01_07/start/index.js
+++ Exercise Files/Ch01/01_07/start/index.js
@@ -8,3 +8,21 @@ var delay = (seconds) =>
   new Promise((resolves) => {
     setTimeout(resolves, seconds * 1000);
   });
+
+Promise.all([
+  writeFile("readme.md", "Hello World"),
+  writeFile("readme.txt", "Hello World"),
+  writeFile("readme.json", '{ "hello": "world" }'),
+])
+  .then(() => readdir(__dirname))
+  .then(console.log)
+  .then(() => {
+    Promise.all([
+      unlink("readme.md"),
+      unlink("readme.txt"),
+      delay(3),
+      unlink("readme.json"),
+    ])
+      .then(() => readdir(__dirname))
+      .then(console.log);
+  });
