b9f56bc06cf1fb4554444968d25a7678a77fafe5
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-27 20:12:44 +0500

    pausing when there is backpressure... resuming when it is drained

--- Exercise Files/Ch02/02_05/start/index.js
+++ Exercise Files/Ch02/02_05/start/index.js
@@ -4,7 +4,11 @@ const readStream = createReadStream("../../powder-day.mp4");
 const writeStream = createWriteStream("./copy.mp4");
 
 readStream.on("data", (chunk) => {
-  writeStream.write(chunk);
+  const result = writeStream.write(chunk);
+  if (!result) {
+    console.log("backpressure");
+    readStream.pause();
+  }
 });
 
 readStream.on("error", (error) => {
@@ -13,6 +17,11 @@ readStream.on("error", (error) => {
 
 readStream.on("end", () => {
   writeStream.end();
+});
+
+writeStream.on("drain", () => {
+  console.log("drained");
+  readStream.resume();
 });
 
 writeStream.on("close", () => {
