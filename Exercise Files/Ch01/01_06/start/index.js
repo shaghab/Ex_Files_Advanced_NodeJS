7f72b699b458df9e6982c6363af259bd35dfd97e
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-27 11:05:33 +0500

    1.6 obtaining data from async functions

--- Exercise Files/Ch01/01_06/start/index.js
+++ Exercise Files/Ch01/01_06/start/index.js
@@ -2,33 +2,13 @@ var fs = require("fs");
 var { promisify } = require("util");
 var writeFile = promisify(fs.writeFile);
 var unlink = promisify(fs.unlink);
+var readdir = promisify(fs.readdir);
 var beep = () => process.stdout.write("\x07");
 var delay = (seconds) =>
   new Promise((resolves) => {
     setTimeout(resolves, seconds * 1000);
   });
 
-const doStuffSequentially = async () => {
-  console.log("starting");
-  await delay(1);
-  console.log("waiting");
-  await delay(2);
-  try {
-    await writeFile("file.txt", "Sample File...");
-    beep();
-  } catch (error) {
-    console.error(error);
-  }
-  console.log("file.txt created");
-  await delay(2);
-  await unlink("file.txt");
-  beep();
-  console.log("file.txt removed");
-
-  return Promise.resolve();
-};
-
-doStuffSequentially()
-  .then(() => console.log("again again!!!"))
-  .then(() => doStuffSequentially())
-  .then(() => console.log("enough already..."));
+readdir(__dirname).then((files) => {
+  console.log(files);
+});
